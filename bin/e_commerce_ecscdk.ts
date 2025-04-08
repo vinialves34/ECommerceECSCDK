30a5238e6092cb55ab02a859f1e8ff1b6f018951
Vinicius Alves <vinialves.dev@gmail.com>
2025-04-08 20:41:46 -0300

    feat: create cluster stack

--- bin/e_commerce_ecscdk.ts
+++ bin/e_commerce_ecscdk.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EcrStack } from '../lib/ecr-stack';
 import { VpcStack } from '../lib/vpc-stack';
+import { ClusterStack } from '../lib/cluster-stack';
 
 const app = new cdk.App();
 
@@ -19,9 +20,16 @@ const tagsInfra = {
 const ecrStack = new EcrStack(app, "Ecr", {
   env: env,
   tags: tagsInfra
-})
+});
 
 const vpcStack = new VpcStack(app, "Vpc", {
   env: env,
   tags: tagsInfra
+});
+
+const clusterStack = new ClusterStack(app, "Cluster", {
+  env: env,
+  tags: tagsInfra,
+  vpc: vpcStack.vpc
 })
+clusterStack.addDependency(vpcStack);
